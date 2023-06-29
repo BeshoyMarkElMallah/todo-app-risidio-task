54560beacbbf1c9a7027b9d91eaf6dffb49099f6
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-06-29 23:09:02 +0300

    change img url

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -7,7 +7,7 @@ const Title = ({ title }) => {
     <div className="py-5 text-2xl font-bold text-white ">
       <div className="flex flex-col items-center justify-center gap-1">
         <img
-          src="public/rlogo.jpg"
+          src="https://github.com/BeshoyMarkElMallah/todo-app-risidio-task/blob/121a5350248d57ff7791a4ecc8cf776750709703/public/rlogo.jpg"
           alt="todo"
           border="0"
           className="w-1/2 mx-1 my-4 rounded-lg"
