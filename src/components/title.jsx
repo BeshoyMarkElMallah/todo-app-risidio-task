121a5350248d57ff7791a4ecc8cf776750709703
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-06-29 23:05:57 +0300

    change logo folder

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -7,7 +7,7 @@ const Title = ({ title }) => {
     <div className="py-5 text-2xl font-bold text-white ">
       <div className="flex flex-col items-center justify-center gap-1">
         <img
-          src="src/assets/rlogo.jpg"
+          src="public/rlogo.jpg"
           alt="todo"
           border="0"
           className="w-1/2 mx-1 my-4 rounded-lg"
